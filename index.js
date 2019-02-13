ecdd5ffacb0bb6cf7de38bf5d98c1bcb74981342
xiechao <xiechao06>
2019-02-13 19:50:05 +0800

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-module.exports = async function oapply (obj, ...funcs) {
+import '@babel/polyfill'
+
+async function applyWith (obj, ...funcs) {
   obj = await Promise.resolve(obj)
   for (let func of funcs) {
     await Promise.resolve(func(obj))
   }
   return obj
 }
+
+export default applyWith
--- test.js
+++ test.js
@@ -1,19 +1,21 @@
-require('should')
-const oapply = require('./')
+import applyWith from './index'
+import test from 'tape'
 
-describe('oapply', function () {
-  it('synchronous', async function () {
-    await oapply(
-      { a: 1 },
-      it => ++it.a,
-      it => { it.a += 2 }
-    ).should.be.resolvedWith({ a: 4 })
-  })
+test('synchronous', async function (t) {
+  let o = await applyWith(
+    { a: 1 },
+    it => ++it.a,
+    it => { it.a += 2 }
+  )
+  t.deepEqual(o, { a: 4 })
+  t.end()
+})
 
-  it('asynchronous', async function () {
-    await oapply(
-      Promise.resolve({ a: 1 }),
-      it => Promise.resolve(4).then(n => { it.a = n })
-    ).should.be.resolvedWith({ a: 4 })
-  })
+test('asynchronous', async function (t) {
+  let o = await applyWith(
+    Promise.resolve({ a: 1 }),
+    it => Promise.resolve(4).then(n => { it.a = n })
+  )
+  t.deepEqual(o, { a: 4 })
+  t.end()
 })
